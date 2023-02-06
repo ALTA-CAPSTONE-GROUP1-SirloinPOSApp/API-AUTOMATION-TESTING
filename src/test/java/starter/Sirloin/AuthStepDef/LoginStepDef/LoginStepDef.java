59e76069c44792f0ccc5eb029a6527c140d5acf9
wyrabin <wirabint.9419@gmail.com>
2023-02-06 22:41:14 +0700

    update feature login

--- src/test/java/starter/Sirloin/AuthStepDef/LoginStepDef/LoginStepDef.java
+++ src/test/java/starter/Sirloin/AuthStepDef/LoginStepDef/LoginStepDef.java
@@ -12,15 +12,17 @@ import starter.Sirloin.Utils.Constant;
 import starter.Sirloin.Utils.SirloinResponses;
 
 import java.io.File;
+
 import static org.hamcrest.Matchers.equalTo;
 
 public class LoginStepDef {
 
     @Steps
     AuthAPI authAPI;
+
     @Given("Post login with valid email and password")
     public void postLoginWithValidEmailAndPassword() {
-        File json = new File(Constant.LOGIN_JSON_REQUEST+"/validEmailPass.json");
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/validEmailPass.json");
         authAPI.postLogin(json);
     }
 
@@ -34,6 +36,7 @@ public class LoginStepDef {
     public void shouldReturnStatusCode(int statusCode) {
         SerenityRest.then().statusCode(statusCode);
     }
+
     @And("Response body page should be {int}, {string}, {string}, {string}, {string}, {string} and {string}")
     public void responseBodyPageShouldBeIdAnd(int id, String business_name, String email, String address, String phone_number, String token, String message) {
         SerenityRest.then()
@@ -48,8 +51,66 @@ public class LoginStepDef {
 
     @And("Validate json schema login")
     public void validateJsonSchemaLogin() {
-        File jsonSchema = new File(Constant.LOGIN_JSON_SCHEMA+"/validEmailPassSchema.json");
+        File jsonSchema = new File(Constant.LOGIN_JSON_SCHEMA + "/validEmailPassSchema.json");
         SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
     }
 
+    @Given("Post login with invalid email and password")
+    public void postLoginWithInvalidEmailAndPassword() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/invalidEmailValidPass.json");
+        authAPI.postLogin(json);
+    }
+
+    @And("Response body page should be {string}")
+    public void responseBodyPageShouldBe(String message) {
+        SerenityRest.then().body(SirloinResponses.MESSAGE, equalTo(message));
+    }
+
+    @And("Validate json schema login negative case")
+    public void validateJsonSchemaLoginNegativeCase() {
+        File jsonSchema = new File(Constant.LOGIN_JSON_SCHEMA + "/negativeCaseLoginSchema.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(jsonSchema));
+    }
+
+    @Given("Post login with empty email and valid password")
+    public void postLoginWithEmptyEmailAndValidPassword() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/emptyEmailValidPass.json");
+        authAPI.postLogin(json);
+    }
+
+    @Given("Post login with valid email and invalid password")
+    public void postLoginWithValidEmailAndInvalidPassword() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/validEmailInvalidPass.json");
+        authAPI.postLogin(json);
+    }
+
+    @Given("Post login with empty email and password")
+    public void postLoginWithEmptyEmailAndPassword() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/emptyEmailPass.json");
+        authAPI.postLogin(json);
+    }
+
+    @Given("Post login with wrong combination email and password")
+    public void postLoginWithWrongCombinationEmailAndPassword() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/wrongCombinationEmailPass.json");
+        authAPI.postLogin(json);
+    }
+
+    @Given("Post login with unregistered email")
+    public void postLoginWithUnregisteredEmail() {
+        File json = new File(Constant.LOGIN_JSON_REQUEST + "/unregisteredEmail.json");
+        authAPI.postLogin(json);
+    }
+
+    @Given("Post login with invalid path {int}")
+    public void postLoginWithInvalidPath(int id) {
+        File json = new File(Constant.LOGIN_JSON_REQUEST+"/validEmailPass.json");
+        authAPI.invalidLoginPath(id,json);
+    }
+
+    @When("Send request login invalid path")
+    public void sendRequestLoginInvalidPath() {
+        SerenityRest.when().post(AuthAPI.LOGIN_INVALID_PATH);
+    }
 }
+
